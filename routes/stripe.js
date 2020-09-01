f36af2cdd5b7dc39e2ded1e1ac51aab2182e5007
Jekynss <Jekyns@mail.ru>
2020-09-01 11:22:38 +0300

    Final stripe integration

--- routes/stripe.js
+++ routes/stripe.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 const AuthMiddl = require("../middlewares/Authentication");
 
-const { handleHook,handleCreateSubscription } = require("../controllers/Stripe");
+const {
+  handleHook,
+  handleCreateSubscription,
+} = require("../controllers/Stripe");
 
 router.route("/stripe-webhook").post(handleHook);
 
 router.route("/subscriptions").post(handleCreateSubscription);
+
+router.route("/secret").get(AuthMiddl.GetAuthWithoutStripe, async (req, res) => {
+  res.json({ client_secret: process.env.STRIPE_PUBLIC_KEY });
+});
 
 module.exports = router;
